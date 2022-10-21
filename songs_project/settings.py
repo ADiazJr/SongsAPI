8e6e72ba063b8759b2de8687f85a1cd3f6a497c6
unknown <arturodiazmay@gmail.com>
2022-10-21 15:39:30 -0500

    Created Song Model for songs App + Registered songs And Rest Framework in Settings Installed Apps Section

--- songs_project/settings.py
+++ songs_project/settings.py
@@ -34,8 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysql',
     'corsheaders',
+    'rest_framework',
+    'songs',
+
 ]
 
 MIDDLEWARE = [
